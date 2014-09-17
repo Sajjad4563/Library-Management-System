05349e0636bb5b51f5cb0deb391c73cb8f5b16fd
Sajjad HTLO <MyBeautifulFuture@gmail.com>
2014-09-17 10:50:40 -0800

    Create SetMyImage.java

---
+++ SetMyImage.java
@@ -0,0 +1,22 @@
+package Project;
+
+import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.JPanel;
+
+public class SetMyImage extends JPanel {
+
+    private final Image img;
+
+    public SetMyImage(Image img) {
+        this.img = img;
+        this.setLayout(new BorderLayout());
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(img,00,00,null);
+    }
+}
